6e2e06231bb4d713f29a2891cd8c94be5f31d354
Javi <jvrglrschz@gmail.com>
2018-01-10 23:15:39 +0100

    Runge-Kutta

---
+++ sintáxix_fortran.f
@@ -0,0 +1,17 @@
+	program nombre
+	integer a-b
+	real	m-i
+	open(unit=number,file="file_name",status="unknown")
+	close(number)
+
+	read from file (unknown end of file) IOSTAT "=" INTEGER VARIABLE!!
+	DO
+   READ(*,*,IOSTAT=Reason)  a, b, c
+   IF (Reason > 0)  THEN
+      ... something wrong ...
+   ELSE IF (Reason < 0) THEN
+      ... end of file reached ...
+   ELSE
+      ... do normal stuff ...
+   END IF
+END DO
