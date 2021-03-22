d01f10cf2d435fc60588f7bc59a51305d85a7cef
Javier Aguilar Sánchez <jvrglrschz@gmail.com>
2021-03-22 11:34:05 +0100

    Add files via upload

---
+++ Fortran/main_SIS.f95
@@ -0,0 +1,14 @@
+program Main_SIS
+  !Main program, depends on the rest of modules (declarations_module, functions and subroutines))
+  use declarations_module
+  use functions
+  use subroutines
+  implicit none
+
+  call assingments()
+  !Print variables from assignments()
+
+  !Do stuff
+
+  !Deallocate and close files
+101 end program Main_SIS
